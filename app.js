5864a3c55c812bfa23678368c06547eb58a58754
LiudmylaBolotova <liudmylabolotova@gmail.com>
2023-12-07 19:51:42 +0200

    fix

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-// const moment = require("moment");
 const fs = require("fs/promises");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,15 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/users", usersRouter);
-
-// app.use(async (req, res, next) => {
-//   const { method, url } = req;
-//   const date = moment().format("DD-MM-YYYY_hh:mm:ss");
-
-//   await fs.appendFile("./public/server.log", `\n${method} ${url} ${date}`);
-
-//   next();
-// });
 
 app.use(async (req, res) => {
   res.status(404).json({
