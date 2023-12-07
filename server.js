93a8967eba2753b470a578fcba23529e95db9320
LiudmylaBolotova <liudmylabolotova@gmail.com>
2023-12-07 22:22:51 +0200

    fix

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./app");
 const mongoose = require("mongoose");
-const { DB_HOST } = process.env;
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
@@ -8,7 +8,7 @@ mongoose
   .connect(DB_HOST)
   .then(() => {
     console.log("Database connection successful");
-    app.listen(3001);
+    app.listen(PORT);
   })
   .catch((error) => {
     console.log(error.message);
