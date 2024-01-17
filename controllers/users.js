70c16fbd0cbb54ee0f4be506d5ad344a7304dd19
LiudmylaBolotova <liudmylabolotova@gmail.com>
2024-01-17 20:48:29 +0200

    Adding validation schemes and functionality
    Added new functionality for routes, added Joy and Mongoose validation schemes

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const {User} = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
@@ -6,44 +6,61 @@ const getAll = async (req, res) => {
   res.json(result);
 };
 
-// const getById = async (req, res, next) => {
-//   const { id } = req.params;
-//   const result = await users.getById(id);
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(result);
-// };
+const getFilter = async (req, res) => {
+  const result = await User.find({}, "name email");
+  res.json(result);
+};
+
+
+const getById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await User.findById(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
 const add = async (req, res) => {
   const result = await User.create(req.body);
   res.status(201).json(result);
 };
 
-// const updateById = async (req, res, next) => {
-//   const { id } = req.params;
-//   const result = await users.updateById(id, req.body);
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json(result);
-// };
+const updateById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await User.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json(result);
+};
 
-// const deleteById = async (req, res, next) => {
-//   const { id } = req.params;
-//   const result = await users.deleteById(id);
-//   if (!result) {
-//     throw HttpError(404, "Not found");
-//   }
-//   res.json({
-//     message: "Delete success",
-//   });
-// };
+const updateStatusUser = async (req, res) => {
+  const { id } = req.params;
+  const result = await User.findByIdAndUpdate(id, req.body, { new: true });
+  if (!result) {
+    throw HttpError(404, "Missing field favorite");
+  }
+  res.json(result);
+};
+
+const deleteById = async (req, res, next) => {
+  const { id } = req.params;
+  const result = await User.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Delete success",
+  });
+};
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  // getById: ctrlWrapper(getById),
+  getFilter: ctrlWrapper(getFilter),
+  getById: ctrlWrapper(getById),
   add: ctrlWrapper(add),
-  // updateById: ctrlWrapper(updateById),
-  // deleteById: ctrlWrapper(deleteById),
+  updateById: ctrlWrapper(updateById),
+  updateStatusUser: ctrlWrapper(updateStatusUser),
+  deleteById: ctrlWrapper(deleteById),
 };
--- models/user.js
+++ models/user.js
@@ -2,36 +2,57 @@ const { Schema, model } = require("mongoose");
 const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
 
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const phoneRegex = /^\+\d{1,4}\d{6,}$/;
+
 const userSchema = new Schema(
   {
     name: {
       type: String,
-      required: [true, "Set name for contact"],
+      minlength: 3,
+      maxlength: 25,
+      required: true,
     },
     email: {
       type: String,
+      match: emailRegex,
+      required: true,
     },
     phone: {
       type: String,
+      match: phoneRegex,
+      required: true,
     },
-   
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: 'user',
+    }
   },
   { versionKey: false }
 );
 
-// userSchema.post("save", handleMongooseError);
+userSchema.post("save", handleMongooseError);
 
 const addSchema = Joi.object({
   name: Joi.string().min(3).max(25).required(),
-  email: Joi.string().min(5).max(255).required().email(),
+  email: Joi.string().regex(emailRegex).required().email(),
   phone: Joi.string()
-    .regex(/^[0-9]{12}$/)
+    .regex(phoneRegex)
     .required(),
+  favorite: Joi.boolean(),
+});
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required(),
 });
 
 const schemas = {
   addSchema,
-
+  updateFavoriteSchema,
 };
 
 const User = model("user", userSchema);
